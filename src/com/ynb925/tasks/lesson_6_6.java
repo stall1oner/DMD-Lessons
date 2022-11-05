2c6913dcd8a413ebf4ab441d19ebd465fb85ae42
stall1oner <thestall1oner@gmail.com>
2022-11-05 20:15:32 +0200

    commit

--- src/com/ynb925/tasks/lesson_6_6.java
+++ src/com/ynb925/tasks/lesson_6_6.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class lesson_6_6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 2x зна-ое положительное число : ");
+        System.out.println("Введите число : ");
         int n = scanner.nextInt();
         if (n > 0) {
             System.out.println("Положительное");
