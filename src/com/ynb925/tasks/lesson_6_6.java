e7280211691bdfe710467cb34641fa1cf3ba1b62
stall1oner <thestall1oner@gmail.com>
2022-11-05 20:10:01 +0200

    commit

---
+++ src/com/ynb925/tasks/lesson_6_6.java
@@ -0,0 +1,26 @@
+package com.ynb925.tasks;
+
+import java.util.Scanner;
+
+/**
+ * Задание 6.6 Напишите класс, который принимает с клавиатуры целое число и выводит на экран
+ * одно из следующих сообщений: Положительное, или Отрицательное, или Ноль, -в зависимости от значения числа.
+ */
+
+public class lesson_6_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите 2x зна-ое положительное число : ");
+        int n = scanner.nextInt();
+        if (n > 0) {
+            System.out.println("Положительное");
+
+        }
+        else if (n < 0) {
+            System.out.println("Отрицательное");
+
+        } else
+            System.out.println("Ноль");
+
+    }
+}
