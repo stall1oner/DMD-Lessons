83f36fca2a2d800303038defa20ebd8991af665f
stall1oner <thestall1oner@gmail.com>
2022-11-05 20:28:57 +0200

    commit

---
+++ src/com/ynb925/tasks/lesson_6_11.java
@@ -0,0 +1,12 @@
+package com.ynb925.tasks;
+
+import java.util.Scanner;
+
+public class lesson_6_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число : ");
+        int n = scanner.nextInt();
+
+        }
+    }
