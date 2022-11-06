ef9917cd47903f61fab7ef385685e35fad4fe706
stall1oner <thestall1oner@gmail.com>
2022-11-07 01:45:06 +0200

    commit

---
+++ src/com/ynb925/tasks/Nastya.java
@@ -0,0 +1,13 @@
+/*package com.ynb925.tasks;
+
+
+public class Nastya {
+    public static void main(String[] args) {
+        string a = "Я";
+        string b = "тебя";
+        string c = "люблю";
+
+        System.out.println(a + b + c);
+
+    }
+}*/
