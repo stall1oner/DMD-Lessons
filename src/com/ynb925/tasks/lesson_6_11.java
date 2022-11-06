bf16224b42c4450873711ff53db4e0b8a7cffcb7
stall1oner <thestall1oner@gmail.com>
2022-11-06 19:58:36 +0200

    commit

--- src/com/ynb925/tasks/lesson_6_11.java
+++ src/com/ynb925/tasks/lesson_6_11.java
@@ -9,14 +9,19 @@ import java.util.Scanner;
  */
 public class lesson_6_11 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число : ");
-        int n = scanner.nextInt();
-        int a = 11;
-        int b = n / a;
-        if (b == 0) {
-            System.out.println("Один");
-        } else if (b != 0) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите число 1 : ");
+        int a = scanner1.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Введите число 2 : ");
+        int b = scanner2.nextInt();
+        if (a == 0) {
+            System.out.println("Некорректно");
+        } else if (b == 0) {
+            System.out.println("Некорректно");
+        } else if (a == b) {
+            System.out.println("Один и тот же знак");
+        }else {
             System.out.println("Разные знаки");
         }
 
