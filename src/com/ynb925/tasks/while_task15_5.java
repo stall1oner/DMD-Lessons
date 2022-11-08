37cb66a48c68e842ae13de259dff649e3f8129f1
stall1oner <thestall1oner@gmail.com>
2022-11-09 01:10:58 +0200

    commit

--- src/com/ynb925/tasks/while_task15_5.java
+++ src/com/ynb925/tasks/while_task15_5.java
@@ -1,7 +1,27 @@
 package com.ynb925.tasks;
 
+import java.util.Scanner;
+
+/**
+ * Задание 15.5  Напишите класс, который принимает с клавиатуры числа,
+ * пока не будет введено значение 999.
+ * Класс должен подсчитать, сколько значений было введено с клавиатуры
+ * (не считая значения 999), и вывести эту информацию на экран.
+ */
 public class while_task15_5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число :");
+        int value1 = scanner.nextInt();
+        int value2 = 0;
+        int rightValue = 999;
+
+
+        while(value1 == rightValue) {
+            value2++;
+            scanner.nextInt();
+        }
+        System.out.println(value2);
 
     }
 }
