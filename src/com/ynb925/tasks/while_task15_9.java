00f6ba95e9f6df7635144c68289e54cee0370946
stall1oner <thestall1oner@gmail.com>
2022-11-10 23:04:43 +0200

    commit

---
+++ src/com/ynb925/tasks/while_task15_9.java
@@ -0,0 +1,24 @@
+package com.ynb925.tasks;
+
+import java.util.Scanner;
+
+/**
+ * Задание 15.9 Напишите класс, который принимает с клавиатуры целое число,
+ * а за тем выводит на экран последующие ему целые числа,
+ * пока сумма этих чисел не превышает квадрата введенного числа.
+ */
+
+public class while_task15_9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int n = scanner.nextInt();
+        int value = n;
+        while (n < value * value) {
+            n++;
+            System.out.println(n);
+        }
+    }
+}
+
+// пи*дец тут подъебка, шо надо отдельно выводить ввод в отдельную переменную, комп загудел от вычеслений)))
