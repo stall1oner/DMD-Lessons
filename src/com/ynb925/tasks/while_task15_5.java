b460e498e910bcd038b661d9d4f27a19c6b2b059
stall1oner <thestall1oner@gmail.com>
2022-11-10 22:17:09 +0200

    commit

--- src/com/ynb925/tasks/while_task15_5.java
+++ src/com/ynb925/tasks/while_task15_5.java
@@ -11,8 +11,6 @@ import java.util.Scanner;
 public class while_task15_5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
         System.out.println("Введите число :");
         int inNum = scanner.nextInt();
         int count = 0;
