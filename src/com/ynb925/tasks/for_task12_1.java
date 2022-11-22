e7dea3cc34d90834d37e6e624aa8db3e978350d2
stall1oner <thestall1oner@gmail.com>
2022-11-22 23:33:07 +0200

    commit

--- src/com/ynb925/tasks/for_task12_1.java
+++ src/com/ynb925/tasks/for_task12_1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Напишите класс, который принимает с клавиатуры 10  целых чисел и выводит на экран информацию о том,
  * сколько из них были положительными.
  */
-public class for_task12_1 {                         // считает больше чем надо, посмотреть решение ярика
+public class for_task12_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число :");
@@ -15,8 +15,8 @@ public class for_task12_1 {                         // считает больш
         int pos = 0;
         int neg = 0;
 
-        for (int i = counter; i != 10; i++) {
-           int inNum = scanner.nextInt();
+        for (int i = counter; i < 10; i++) {
+            int inNum = scanner.nextInt();
             if (inNum > 0) {
                 pos++;
             } else neg++;
