eb1f4649773891963b30c9a02f17952eed61fb91
stall1oner <thestall1oner@gmail.com>
2022-11-22 22:55:37 +0200

    commit

---
+++ src/com/ynb925/tasks/for_task11_27.java
@@ -0,0 +1,36 @@
+package com.ynb925.tasks;
+
+import java.util.Scanner;
+
+/**
+ * Задание 11.27 звучит интерестно, по желанию
+ * Напишите класс, который принимает с клавиатуры целое положительное число,
+ * а затем выводит на экран все целые положительные двузначные числа, сумма цифр которых равна введенному с клавиатуры значению.
+ */
+
+public class for_task11_27 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число : ");
+        int sumOfDigits = scanner.nextInt();
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+
+        for (int i = 1; i < 100; i++) {
+            a = i / 10;
+            b = i % 10;
+            c = a + b;
+            if (c != sumOfDigits){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+
+
