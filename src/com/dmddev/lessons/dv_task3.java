a0aec4f7d65b9566a5d3a3797fd9ee56c1eb2c61
stall1oner <thestall1oner@gmail.com>
2022-11-27 18:55:39 +0200

    commit

---
+++ src/com/dmddev/lessons/dv_task3.java
@@ -0,0 +1,16 @@
+package com.dmddev.lessons;
+
+public class dv_task3 {
+    public static void main(String[] args) {
+        double money = 24;
+        double year = 1627;
+        double percent = 0.05;
+
+        for (double i = year; i < 2022; i++) {
+            money = money + (money * percent);
+            System.out.println(i + " " + money);
+        }
+
+
+    }
+}
