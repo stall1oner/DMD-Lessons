5663be69ab9150966efb8f0c6d782c334c8ef67d
stall1oner <thestall1oner@gmail.com>
2022-11-28 21:56:04 +0200

    commit

---
+++ src/com/ynb925/tasks/array_tasks/array_task_18_12.java
@@ -0,0 +1,46 @@
+package com.ynb925.tasks.array_tasks;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Задание 18.12 Напишите класс, который заполняет случайными трехзначными числами массив,
+ * размер которого при инициализации вводится с клавиатуры. После заполнения класс должен
+ * вывести на экран значения массива в одной строке, начиная с «начала» массива (с первой ячейки).
+ */
+
+public class array_task_18_12 {
+    public static void main(String[] args) {
+        random();
+
+
+    }
+
+    public static int arrayTask() {
+        int[] values = {};
+        int length = values.length;
+        length = scannerInt();
+
+
+        for (int i = 0; i < 5; i++) {
+
+        }
+
+        return length;
+    }
+
+    public static int scannerInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        return scanner.nextInt();
+    }
+
+    public static int random() {
+        int min = 0;
+        int max = 100;
+        int diff = max - min;
+        Random random = new Random();
+        return random.nextInt(diff + 1);
+    }
+
+}
