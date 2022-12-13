44d38034d36142b13ec14fe0c4e8d77b52f640c7
stall1oner <thestall1oner@gmail.com>
2022-12-14 01:58:51 +0200

    commit

---
+++ src/com/dmddev/lessons/com/dmdev/lesson7.java
@@ -0,0 +1,14 @@
+package com.dmddev.lessons.com.dmdev;
+
+public class lesson7 {
+    public static void main(String[] args) {
+        int[] c2 = new int[10];
+        for (int i = 0; i < c2.length; i++) {
+            System.out.println(i + " :" + c2[i]);
+        }
+
+        int[] values = {1, 2, 3, 4, 5};
+
+
+    }
+}
---
+++ src/com/javarush/task/jdk13/task06/task0634/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task06.task0634;
+
+import java.util.Scanner;
+
+/*
+Шахматная доска
+*/
+
+public class Solution {
+    public static char[][] array;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
